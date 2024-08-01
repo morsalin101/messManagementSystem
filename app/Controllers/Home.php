946981dce4efa6fe6d48f67b986efcec56858e26
morsalin101 <57835317+morsalin101@users.noreply.github.com>
2024-08-01 13:21:32 +0600

    push

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('admin/partials/header')
+        .view('admin/partials/top')
+        .view('admin/partials/sidebar')
+         .view('admin/index')
+         .view('admin/partials/footer');
     }
 }
